e9b9a02bec6cbbc914dfd39ebff9457f358b5ef7
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-07-13 17:43:51 -0400

    get status request

--- utils/blogUpdaterRequester/blogPostRequester.ts
+++ utils/blogUpdaterRequester/blogPostRequester.ts
@@ -9,6 +9,15 @@ interface DeleteBlogPostRequest extends BaseRequest {
 	post_name: string;
 }
 
+interface UpdateBlogPostReleaseRequest extends BaseRequest {
+	post_name: string;
+	release: boolean;
+}
+
+interface StatusBlogPostRequest extends BaseRequest {
+	post_name: string;
+}
+
 export class BlogPostRequester extends BlogUpdaterRequester {
 	constructor(
 		token_granter_wrapper: TokenGranterWrapper,
@@ -86,11 +95,64 @@ export class BlogPostRequester extends BlogUpdaterRequester {
 	): Promise<boolean> {
 		const updateReleaseUrl = `${this.blog_updater_url}/blogPosts/updatePostRelease`;
 
+		const request: UpdateBlogPostReleaseRequest = {
+			...(await this.token_granter_wrapper.getBaseRequest()),
+			post_name: blogPostName,
+			release,
+		};
+
+		try {
+			const response = await axios.post(updateReleaseUrl, request, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (response.status === 200) {
+				console.log("Released blog post:", blogPostName);
+				return true;
+			} else {
+				console.log(
+					"Failed to update blog post release, error:",
+					response.statusText
+				);
+				return false;
+			}
+		} catch (error) {
+			console.log("Failed to update blog post release:", error);
+		}
 		return false;
 	}
 
 	async getStatus(blogPostName: string): Promise<ImageStatus> {
 		const statusUrl = `${this.blog_updater_url}/blogPosts/blogPostStatus`;
+
+		const request: StatusBlogPostRequest = {
+			...(await this.token_granter_wrapper.getBaseRequest()),
+			post_name: blogPostName,
+		};
+
+		try {
+			const response = await axios.post(statusUrl, request, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (response.status >= 400) {
+				return {
+					released: false,
+					published: false,
+				};
+			} else {
+				const formattedResponse: ImageStatus = JSON.parse(
+					response.data
+				);
+				return formattedResponse;
+			}
+		} catch (error) {
+			console.log("Failed to get post status:", error);
+		}
 
 		return {
 			released: false,
